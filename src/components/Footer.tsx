fc64f0c6c6b2bccb1828b0c597701069503101e8
ajay <negiajay354@gmail.com>
2024-05-16 10:00:46 +0530

    chng

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,25 @@
 export default function Footer() {
   return (
     <footer className="min-w-full bg-gray-800 p-4 text-center text-gray-500">
-      <small 
-        className="block text-gray-400 text-sm mt-1 mb-2"
-      >&copy; 2024 Ajay Negi</small>
+      <small className="mb-2 mt-1 block text-sm text-gray-400">
+        &copy; 2024 Ajay Negi
+      </small>
       <p>
-        <span
-            className="text-gray-400 font-semibold"
-        >About this website :</span> built with Next.js(App Router & Server
-        Actions), Tailwind CSS, Typescript , Framer Motion , Vercel hosting.
+        <span className="font-semibold text-gray-400">
+          About this website :
+        </span>{" "}
+        built with Next.js(App Router & Server Actions), Tailwind CSS,
+        Typescript , Framer Motion , Vercel hosting.
+      </p>
+      <p>
+        Inspired by{" "}
+        <a
+          href="https://youtu.be/sUKptmUVIBM?si=xSVwkwagrozBMgyn"
+          className="text-blue-400"
+          target="_blank"
+        >
+          ByteGrade
+        </a>
       </p>
     </footer>
   );
